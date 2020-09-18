9b0826025157ef7f3e8f71f0c9911b0a43f54f88
Rajeev Atla <rajeev@rajeevatla.com>
2020-09-18 03:35:40 +0000

    Update

---
+++ load_data.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+DATA_URL = 'https://raw.githubusercontent.com/RajeevAtla/Superconductivity-Dataset/master/dataset.csv'
+
+
+def load_data():
+    return pd.read_csv(DATA_URL)
+
+#data = load_data()
