4f779d4d8f6e4562058033ff0e5f94eabebacf11
Rajeev Atla <rajeev@rajeevatla.com>
2021-02-23 00:18:22 -0500

    Update test_1.py

--- tests/test_1.py
+++ tests/test_1.py
@@ -10,7 +10,6 @@ def test1():
     """
     Test that will instantiate a model object and fit it.
     """
-    print(SuperconGAN.__version__)
     model = SuperconGAN.Synthesizer()
     model.fit()
     pass
